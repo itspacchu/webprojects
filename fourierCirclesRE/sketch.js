1f88e9ef436aaf48cb3f74a4e7a377b044bc665a
itspacchu <nandipati.prashant1509@gmail.com>
2020-08-14 15:06:01 +0530

    Fourier

--- fourierCirclesRE/sketch.js
+++ fourierCirclesRE/sketch.js
@@ -1,12 +1,16 @@
-function setup() {
-  width = 400;
-  height = 400;
-  t = 0
-  cn = [20,55,30,55,20,3,4,12,4,3,1,5,4,3,1,5,3,2,1,6,4,3]
-  createCanvas(width, height);
-  tracer = []
+const PI = 3.1415;
 
-  circlecount = 6;
+function setup() {
+  width = 800;
+  height = 800;
+  t = 0
+  
+  sample = [1,1,1,1,1,1];
+  createCanvas(width, height);
+  cn = fourierCalculator(sample,0.01);
+  tracer = []
+  print(cn);
+  circlecount = cn.length;
 }
 
 function draw() {
@@ -20,9 +24,9 @@ function draw() {
   for(let n = 0; n < circlecount;n++){
     i_old = i;
     j_old = j;
-    rad = cn[n];
-    i += rad * cos(n*t);
-    j += rad * sin(n*t);
+    rad = cn[n].mag();
+    i += rad * cos(n*t + cn[n].ang());
+    j += rad * sin(n*t + cn[n].ang());
     stroke(map(n,0,circlecount,50,125));
     strokeWeight(1);
     noFill();
@@ -46,7 +50,8 @@ function draw() {
   }
 
   stroke(255);
-  for(let n = 1 ; n < tracer.length ; n++){
+  for(let n = tracer.length-1 ; n > 0  ; n--){
+    stroke(map(n,0,tracer.length,255,42));
     line(tracer[n-1].x,tracer[n-1].y,tracer[n].x,tracer[n].y)
   }
   //ticking
@@ -54,16 +59,40 @@ function draw() {
 }
 
 
-function drawArrow(base, vec, myColor) {
-  push();
-  stroke(myColor);
-  strokeWeight(3);
-  fill(myColor);
-  translate(base.x, base.y);
-  line(0, 0, vec.x, vec.y);
-  rotate(vec.heading());
-  let arrowSize = 7;
-  translate(vec.mag() - arrowSize, 0);
-  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-  pop();
+function EulerValues(rad,ph){
+  this.rad = rad;
+  this.phase = phase;
+}
+
+
+function fourierCalculator(arr,dt){
+  // Calculates integral from 0-PI
+  let cn = []
+  let ct = 0;
+  let st = 0;
+
+  for(let n=0;n<arr.length;n++){
+    for(let i=0;i<PI;i+=dt){
+      ct+= cos(n*2*PI*i)
+    }
+    for(let i=0;i<PI;i+=dt){
+      st+= sin(n*2*PI*i)
+    }
+    cn.push(new cmpx(ct/arr.length,st/arr.length));
+  }
+  
+  return cn;
+}
+
+function cmpx(real,imag){
+  this.real = real;
+  this.imag = imag;
+
+  this.mag = function(){
+    return sqrt(this.real*this.real + this.imag*this.imag);
+  }
+
+  this.ang = function(){
+    return atan2(this.imag,this.real);
+  }
 }
